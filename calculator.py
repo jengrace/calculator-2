c6890458df31c48b1adbcaee5cd020f177936f9f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-05 12:31:22 -0800

    breaking and infinite variables complete

--- calculator.py
+++ calculator.py
@@ -11,50 +11,6 @@ from arithmetic import *
 # Your code goes here
 
 
-""" 
-while true:
-    input = take user input
-    tokens = input.split(" ")
-    oper = token[0]
-
-
-    if q or quit:
-        quit
-    elif oper in [all operations]:
-        num1 = int(tokens[1]
-        try:
-            num2 = tokens[2]
-            try:
-                num1 = int(num1)
-                num2 = int(num2)
-            except ValueError:
-                print "not valid input"
-                continue
-        except IndexError:
-            try:
-                num1 = int(num1)
-            except ValueError:
-                print "not valid input"
-            num2 = None
-
-        try: 
-
-        if oper == +:
-            arithmetic.add()
-        if ...... rest of operators 
-
-    else:
-        print "sorry i dnt understand"
-        continue
-
-"""
-
-"""
-make one print statement
-let user use upper and lower case
-wrap everything in a function 
-make function for validation 
-"""
 
 
 def validated_tokens(nums):
@@ -75,22 +31,19 @@ while True:
     user_input = raw_input("Enter calculation: \n  >   ")
     tokens = user_input.split(" ")
 
-    oper = tokens[0]
+    oper = tokens[0].lower()
     result = ""
-
 
     if oper in ["q", "quit"]:
         break
-
 
     elif oper in ["+", "-", "*", "/", "square", "cube", "pow", "mod"]:
         nums = tokens[1:]
         int_nums = validated_tokens(nums)
 
-        if int_nums == None:
+        if int_nums is None:
             print "Please only enter integers for us to calculate"
             continue
-
 
         if oper == "+":
             result = add(int_nums)
@@ -100,21 +53,22 @@ while True:
             result = multiply(int_nums)
         elif oper == "/":
             result = divide(int_nums)
-        
-        # elif oper == "pow":
-        #     result = power(num1, num2)
-        # elif oper == "mod":
-        #     result = mod(num1, num2)
-        if oper in ["square", "cube"]:
+
+        elif oper == "pow":
+            result = power(int_nums)
+        elif oper == "mod":
+            result = mod(int_nums)
+
+        elif oper in ["square", "cube"]:
             if len(int_nums) > 1:
-                print "These operations only take one integer"
+                print "These operations only takes one integer"
                 continue
 
-        elif oper == "square":
-            result = square(int_nums)
-            print result
-        elif oper == "cube":
-            result = cube(int_nums)
+            if oper == "square":
+                result = square(int_nums)
+                print result
+            elif oper == "cube":
+                result = cube(int_nums)
 
         print "  =  %s" % (result)
 
