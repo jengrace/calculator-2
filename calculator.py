00286350e90032b9aeb6baaa8ba80f6f9d4e1474
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-05 13:09:30 -0800

    built own reduce function and used it

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+from orig_arithmetic import *
 
 
 # Your code goes here
@@ -27,8 +27,24 @@ def validated_tokens(nums):
     return int_list
 
 
+def my_reduce(func, lst):
+    """ take a function and a list, and iterativly call the function on the list
+    returns end result
+    """
+    if len(lst) == 0:
+        return 0
+
+    result = lst[0]
+    for item in lst[1:]:
+        result = func(result, item)
+
+    return result
+
+
+
 while True:
     user_input = raw_input("Enter calculation: \n  >   ")
+    user_input = user_input.rstrip().lstrip()
     tokens = user_input.split(" ")
 
     oper = tokens[0].lower()
@@ -46,29 +62,31 @@ while True:
             continue
 
         if oper == "+":
-            result = add(int_nums)
+            result = my_reduce(add, int_nums)
         elif oper == "-":
-            result = subtract(int_nums)
+            result = my_reduce(subtract, int_nums)
         elif oper == "*":
-            result = multiply(int_nums)
+            result = my_reduce(multiply, int_nums)
         elif oper == "/":
-            result = divide(int_nums)
+            result = my_reduce(divide, int_nums)
 
         elif oper == "pow":
-            result = power(int_nums)
+            result = my_reduce(power, int_nums)
         elif oper == "mod":
-            result = mod(int_nums)
+            result = my_reduce(mod, int_nums)
 
         elif oper in ["square", "cube"]:
             if len(int_nums) > 1:
                 print "These operations only takes one integer"
                 continue
 
+            single_num = int_nums[0]
+
             if oper == "square":
-                result = square(int_nums)
+                result = square(single_num)
                 print result
             elif oper == "cube":
-                result = cube(int_nums)
+                result = cube(single_num)
 
         print "  =  %s" % (result)
 
