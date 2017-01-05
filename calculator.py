61e7c038c577a421a5337b5641c087b322509449
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 18:06:21 -0800

    takes infinite nums and dryer validation process

--- calculator.py
+++ calculator.py
@@ -56,52 +56,70 @@ wrap everything in a function
 make function for validation 
 """
 
+
+def validated_tokens(nums):
+    """ validates that inputs can be ints and creates a list of them as ints
+    """
+
+    int_list = []
+    for num in nums:
+        try:
+            int_num = int(num)
+            int_list.append(int_num)
+        except ValueError:
+            return None
+    return int_list
+
+
 while True:
     user_input = raw_input("Enter calculation: \n  >   ")
     tokens = user_input.split(" ")
+
     oper = tokens[0]
+    result = ""
+
 
     if oper in ["q", "quit"]:
         break
+
+
     elif oper in ["+", "-", "*", "/", "square", "cube", "pow", "mod"]:
-        num1 = tokens[1]
-        try:
-            num2 = tokens[2]
-            try:
-                num1 = int(num1)
-                num2 = int(num2)
-            except ValueError:
-                print "Please provide integers to calculate"
-                continue
-        except IndexError:
-            try:
-                num1 = int(num1)
-            except ValueError:
-                print "Please provide integers to calculate"
-                continue
-            num2 = None
-        result = ""
+        nums = tokens[1:]
+        int_nums = validated_tokens(nums)
+
+        if int_nums == None:
+            print "Please only enter integers for us to calculate"
+            continue
+
+
         if oper == "+":
-            result = add(num1, num2)
+            result = add(int_nums)
         elif oper == "-":
-            result = subtract(num1, num2)
+            result = subtract(int_nums)
         elif oper == "*":
-            result = multiply(num1, num2)
+            result = multiply(int_nums)
         elif oper == "/":
-            result = divide(num1, num2)
+            result = divide(int_nums)
+        
+        # elif oper == "pow":
+        #     result = power(num1, num2)
+        # elif oper == "mod":
+        #     result = mod(num1, num2)
+        if oper in ["square", "cube"]:
+            if len(int_nums) > 1:
+                print "These operations only take one integer"
+                continue
+
         elif oper == "square":
-            result = square(num1)
+            result = square(int_nums)
+            print result
         elif oper == "cube":
-            result = cube(num1)
-        elif oper == "pow":
-            result = power(num1, num2)
-        elif oper == "mod":
-            result = mod(num1, num2)
-        print "  =  %i" % (result)
+            result = cube(int_nums)
+
+        print "  =  %s" % (result)
 
     else:
         print "Sorry I didnt understand what you wrote"
-
 
 
 
